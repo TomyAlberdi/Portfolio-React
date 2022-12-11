2675b12f9ae0c844c01458f8b2f242490bb2d388
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-11 17:58:54 -0300

    Fin. home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <div className='Home-Presentacion'>
-                <div className="info">
-                    <h2>Título</h2>
+            <div className='Home Home-Presentacion'>
+                <section className="info">
+                    <h1>Título</h1>
                     <p>Pellentesque iaculis consequat diam, id dictum magna pretium volutpat. Sed sapien ex, commodo ut sollicitudin vitae, semper id dolor. Cras congue hendrerit neque, ut accumsan nunc placerat in. Aliquam ac porta nisi, at interdum tortor. Morbi venenatis odio elit, id sagittis neque pellentesque at. Ut ac interdum lacus, in volutpat turpis. Cras tortor est, sodales sed fringilla tincidunt, porttitor in magna. Pellentesque lorem odio, sodales et arcu et, volutpat lacinia augue. Mauris auctor diam ut elementum vehicula. Donec tristique elementum diam vestibulum scelerisque. </p>
                     <ul>
                         <li>data</li>
@@ -13,18 +13,21 @@ const Home = () => {
                         <li>data</li>
                         <li>data</li>
                     </ul>
-                </div>
-                <div className="photo">
-                    <img src="src\assets\pfp.jpg" alt="myself." />
-                </div>
+                </section>
+                <img src="src\assets\pfp.jpg" alt="myself." />
             </div>
-            <div className="Home-Educacion">
-                <div>
-                    <h2>a1</h2>
-                </div>
-                <div>
-                    <h2>a2</h2>
-                </div>
+            <div className="Home Home-Educacion">
+                <h2>Educación</h2>
+                <section className='ed-info'>
+                    <article>
+                        <h3>Educación 1</h3>
+                        <p>Pellentesque iaculis consequat diam, id dictum magna pretium volutpat. Sed sapien ex, commodo ut sollicitudin vitae, semper id dolor.</p>
+                    </article>
+                    <article>
+                        <h3>Educación 2</h3>
+                        <p>Pellentesque iaculis consequat diam, id dictum magna pretium volutpat. Sed sapien ex, commodo ut sollicitudin vitae, semper id dolor.</p>
+                    </article>
+                </section>  
             </div>
         </>
     )
