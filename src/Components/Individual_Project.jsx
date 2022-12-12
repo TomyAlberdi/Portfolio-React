48bf999b00fe3b31b268d8e9d64aa1c0738e537d
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-11 23:19:45 -0300

    Setup proyectos

--- src/Components/Individual_Project.jsx
+++ src/Components/Individual_Project.jsx
@@ -1,26 +1,30 @@
-import { React, useContext, useEffect } from "react";
+import { React, useContext } from "react";
 import { ContextProyecto } from "./utils/projects_context";
 
-const Individual_Project = ({id,img,title,icons}) => {
+const Individual_Project = ({id,img,title,icons,link}) => {
 
     const { States, handleEstado } = useContext(ContextProyecto)
 
+    const handleLink = () => {
+        if (States[id]==="active") {
+            window.open(link,"_blank")
+        }
+    }
+
     return (
-        <article className={"proyecto "+States[id]} onClick={() => handleEstado(id)}>
+        <article className={"proyecto "+States[id]} onClick={() => {handleLink();handleEstado(id);}}>
             <div className="label">
                 <img src={img} alt="test" />
                 <div className="bar">
                     <div className="number">
-                        <h2>{id}</h2>
+                        <h2>{id+1}</h2>
                     </div>
                     <div className="info">
                         <h2>{title}</h2>
                         <div className="icons">
-                            {
-                                icons.map((index, item) => {
-                                    return <i key={index} className={"fa-brands fa-"+icons[item]}></i>
-                                })
-                            }
+                            <i className={"fa-brands fa-"+icons[0]}></i>
+                            <i className={"fa-brands fa-"+icons[1]}></i>
+                            <i className={"fa-brands fa-"+icons[2]}></i>
                         </div>
                     </div>
                 </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,9 +7,8 @@ const Projects = () => {
     return (
         <ContextProvider>
             <div className='Main Projects'>
-                <h1>Proyectos</h1>
                 <section className="container-proyectos">
-                    <Individual_Project key={1} id={0} img='src\assets\time-desktop.png' title="Time Tracking Dashboard" icons={["html5","sass","js"]} />
+                    <Individual_Project key={1} id={0} img='src\assets\time-desktop.png' title="Time Tracking Dashboard" icons={["html5","sass","js"]} link="https://tomyalberdi.github.io/Time-Tracking-Dashboard/" />
                     <Individual_Project key={2} id={1} img='src\assets\time-desktop.png' title="Time Tracking Dashboard" icons={["html5","sass","js"]} />
                     <Individual_Project key={3} id={2} img='src\assets\time-desktop.png' title="Time Tracking Dashboard" icons={["html5","sass","js"]} />
                     <Individual_Project key={4} id={3} img='src\assets\time-desktop.png' title="Time Tracking Dashboard" icons={["html5","sass","js"]} />
