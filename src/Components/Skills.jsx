bbfc8c2bcf1c3afe55b3cf760cd062b43b52012b
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-14 23:49:15 -0300

    Setup skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
+    return (
+        <>
+            <div className="Main frontend">
+                <h2>Frontend</h2>
+            </div>
+            <div className="Main backend">
+                <h2>Backend</h2>
+            </div>
+            <div className="Main infra">
+                <h2>Infraestructura</h2>
+            </div>
+            <div className="Main soft">
+                <h2>Soft Skills</h2>
+            </div>
+        </>
+    )
 }
 
 export default Skills
