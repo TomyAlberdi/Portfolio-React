242a833f4d1464dd91f4158614c10c21563e5170
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-20 18:02:18 -0300

    Quick fix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -20,15 +20,15 @@ const Contact = () => {
             <h1 className='titulo'>Contacto</h1>
             <section className="links">
                 <a href="https://github.com/TomyAlberdi" target="_blank">
-                    <i class="fa-brands fa-github"></i>
+                    <i className="fa-brands fa-github"></i>
                     <h3>GitHub</h3>
                 </a>
                 <a href="https://www.linkedin.com/in/tom%C3%A1s-alberdi-42150a219/" target="_blank">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
                     <h3>LinkedIn</h3>
                 </a>
                 <a href="https://wa.me/+542214181379" target="_blank">
-                    <i class="fa-brands fa-whatsapp"></i>
+                    <i className="fa-brands fa-whatsapp"></i>
                     <h3>WhatsApp</h3>
                 </a>
             </section>
