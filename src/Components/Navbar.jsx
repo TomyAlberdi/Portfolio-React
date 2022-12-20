1a7dadb5e7bad4001b4f11836daac390d7014402
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-20 20:32:26 -0300

    adv media query

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const openNavbar = () => {
+        let nav = document.querySelector('.Navbar')
+        nav.classList.toggle('opened')
+        let icon = document.querySelector('.fa-stack-1x')
+        icon.classList.toggle('fa-bars')
+        icon.classList.toggle('fa-xmark')
+    }
+
     return (
         <>
             <div className='Navbar'>
@@ -11,9 +20,9 @@ const Navbar = () => {
                     <Link to={'/habilidades'}>Habilidades</Link>
                     <Link to={'/contacto'}>Contacto</Link>
                 </div>
-                <Link to={'/'}><img src="src\assets\favicon.ico"/></Link>
+                <Link className='link_icon' to={'/'}><img src="src\assets\favicon.ico"/></Link>
             </div>
-            <span className="open_navbar fa-stack fa-lg">
+            <span className="open_navbar fa-stack fa-lg" onClick={openNavbar}>
                 <i className="fa-solid fa-circle fa-stack-2x"></i>
                 <i className="fa-solid fa-bars fa-stack-1x"></i>
             </span>
