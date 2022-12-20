ff0da08d8a22c7b112cb93c6f1bd948ec614d113
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-12-20 18:39:43 -0300

    strd media query

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='Navbar'>
-            <div>
-                <Link to={'/'}>Inicio</Link>
-                <Link to={'/proyectos'}>Proyectos</Link>
-                <Link to={'/habilidades'}>Habilidades</Link>
-                <Link to={'/contacto'}>Contacto</Link>
+        <>
+            <div className='Navbar'>
+                <div>
+                    <Link to={'/'}>Inicio</Link>
+                    <Link to={'/proyectos'}>Proyectos</Link>
+                    <Link to={'/habilidades'}>Habilidades</Link>
+                    <Link to={'/contacto'}>Contacto</Link>
+                </div>
+                <Link to={'/'}><img src="src\assets\favicon.ico"/></Link>
             </div>
-            <Link to={'/'}><img src="src\assets\favicon.ico"/></Link>
-        </div>
+            <span className="open_navbar fa-stack fa-lg">
+                <i className="fa-solid fa-circle fa-stack-2x"></i>
+                <i className="fa-solid fa-bars fa-stack-1x"></i>
+            </span>
+        </>
     )
 }
 
